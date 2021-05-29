dfc3e48b579cdc6749a851738b9b16e7e1961b7c
Nicole WItherall <nicwitherall@gmail.com>
2021-05-29 14:44:09 -0400

    initialize repo with assets folder stocked with sounds and photos needed

---
+++ src/components/MusicPlayer/MusicPlayer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MusicPlayer (props) {
+    return (
+        <>
+        <h1>Play screen here!</h1>
+        </>
+    )
+}
+
+export default MusicPlayer;
---
+++ src/components/Sound/Sound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Sound (props) {
+    return (
+        <>
+        <h1>Sounds here!</h1>
+        </>
+    )
+}
+
+export default Sound;
