2dd4797cd973d25c9fe9803e69d7d5e80e21ddc0
Nicole WItherall <nicwitherall@gmail.com>
2021-05-29 22:59:13 -0400

    handlePlay function work in progress

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import WavesPhoto from './assets/photos/waves.jpg';
 import WavesSound from './assets/sounds/waves.wav';
 import WindPhoto from './assets/photos/wind.jpg';
 import WindSound from './assets/sounds/wind.mp3';
+import playIcon from './assets/icons/play.png';
+import stopIcon from './assets/icons/stop.png';
 
 
 class App extends Component {
@@ -29,6 +31,8 @@ class App extends Component {
     currentName: "Birds Chirping",
     currentSound: BirdSound,
     currentPhoto: BirdPhoto,
+    isPlaying: false,
+    icon: playIcon,
   }
 
   //  HANDLER FUNCTIONS
@@ -39,6 +43,27 @@ class App extends Component {
       currentPhoto: incomingPhoto
     })
   }
+  handlePlay = () => {
+    let song = new Audio(this.state.currentSound);
+    if (this.state.isPlaying === false) {
+      this.setState({
+        isPlaying: true,
+        icon: stopIcon
+      })
+      song.play();
+      song.addEventListener('ended', function() {
+        song.currentTime = 0;
+        song.play();
+    })
+  } else if (this.state.isPlaying === true) {
+    this.setState({
+      isPlaying: false,
+      icon: playIcon
+    })
+    song.pause();
+    song.currentTime = 0;
+  }
+  }
 
 
   render() {
@@ -48,7 +73,7 @@ class App extends Component {
         <h1>Calmly</h1>
       </header>
       <Sounds sounds={this.state.sounds} handleSound={this.handleSound} />
-      <MusicPlayer currentName={this.state.currentName} currentSound={this.state.currentSound} currentPhoto={this.state.currentPhoto} />
+      <MusicPlayer currentName={this.state.currentName} currentSound={this.state.currentSound} currentPhoto={this.state.currentPhoto} handlePlay={this.handlePlay} icon={this.state.icon} />
     </div>
   );
   }
--- src/components/MusicPlayer/MusicPlayer.js
+++ src/components/MusicPlayer/MusicPlayer.js
@@ -1,15 +1,13 @@
 import React from 'react';
-// IMPORT FILES FROM ASSETS
-import playIcon from '../../assets/icons/play.png';
-import stopIcon from '../../assets/icons/stop.png';
 
 function MusicPlayer (props) {
     return (
         <div className="music-player">
             <img src={props.currentPhoto} />
             <h3>{props.currentName}</h3>
-            <button><img src={playIcon} /></button>
-            <button><img src={stopIcon} /></button>
+            <button
+                onClick={props.handlePlay}
+            ><img src={props.icon} /></button>
         </div>
     )
 }
